b8d2f31f1faaea7cb3080ac6c9b625bb62731d9b
SamA1430 <samuel.arg14@gmail.com>
2025-02-11 19:14:42 -0500

    Put notebooks function in class

---
+++ src/scripts/Processed.py
@@ -0,0 +1,119 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+class Processed:
+    def __init__(self, mvps: pd.DataFrame, standings: pd.DataFrame, advanced: pd.DataFrame, per_game: pd.DataFrame):
+        self.mvps = mvps
+        self.standings = standings
+        self.advanced = advanced
+        self.per_game = per_game
+        self.processed = None
+        
+    def remove_divisions(self) -> pd.DataFrame:
+        self.standings = self.standings[self.standings.Team.str.contains('Division') == False]
+        return self.standings
+    
+    def clean_stats_standings(self) -> pd.DataFrame:
+        self.standings = self.standings.drop(columns=["W","L",'GB', 'PS/G', 'PA/G', 'SRS'])
+        return self.standings
+    
+    def update_team_names(self) -> pd.DataFrame:
+        team_map = {
+            'Atlanta Hawks': 'ATL',
+            'Boston Celtics': 'BOS',
+            'Brooklyn Nets': 'BRK',
+            'Charlotte Hornets': 'CHH',
+            'Chicago Bulls': 'CHI',
+            'Cleveland Cavaliers': 'CLE',
+            'Dallas Mavericks': 'DAL',
+            'Denver Nuggets': 'DEN',
+            'Detroit Pistons': 'DET',
+            'Golden State Warriors': 'GSW',
+            'Houston Rockets': 'HOU',
+            'Indiana Pacers': 'IND',
+            'Los Angeles Clippers': 'LAC',
+            'Los Angeles Lakers': 'LAL',
+            'Memphis Grizzlies': 'MEM',
+            'Miami Heat': 'MIA',
+            'Milwaukee Bucks': 'MIL',
+            'Minnesota Timberwolves': 'MIN',
+            'New Orleans Pelicans': 'NOP',
+            'New York Knicks': 'NYK',
+            'Oklahoma City Thunder': 'OKC',
+            'Orlando Magic': 'ORL',
+            'Philadelphia 76ers': 'PHI',
+            'Phoenix Suns': 'PHO',
+            'Portland Trail Blazers': 'POR',
+            'Sacramento Kings': 'SAC',
+            'San Antonio Spurs': 'SAS',
+            'Toronto Raptors': 'TOR',
+            'Utah Jazz': 'UTA',
+            'Washington Wizards': 'WAS',
+            'Washington Bullets': 'WSB',
+            "New Jersey Nets": "NJN",
+            "Kansas City Kings": "KCK",
+            "San Diego Clippers": "SDC",
+            "Seattle SuperSonics": "SEA",
+            "Vancouver Grizzlies": "VAN",
+            "New Orleans Hornets": "NOH",
+            "Charlotte Bobcats": "CHA",
+            "New Orleans/Oklahoma City Hornets": "NOK",
+        }
+        self.standings['Team'] = self.standings['Team'].map(team_map)
+        return self.standings
+    
+    def clean_mvps(self) -> pd.DataFrame:
+        self.mvps = self.mvps.drop(columns=["Age", "First", "Pts Won", "Pts Max", "WS", "WS/48", "G", "MP", "PTS", "TRB", "AST", "STL", "BLK", "FG%", "3P%", "FT%", "Tm"])
+        return self.mvps
+    
+    def merge_stats(self) -> pd.DataFrame:
+        self.advanced = pd.merge(self.advanced, self.standings[['Year', 'Team', 'W/L%', "seed"]], left_on=['Year', 'Team'], right_on=["Year", "Team"], how='left')
+        self.advanced = self.advanced.drop(columns=["MP"])
+        self.per_game = self.per_game.drop(columns=["Team", "Age", "G", "GS", "Pos", "Awards"])
+        self.advanced = pd.merge(self.advanced, self.per_game, left_on=['Year', 'Player'], right_on=["Year", "Player"], how='left')
+        self.advanced = self.advanced.drop_duplicates(subset=['Player', 'Year'], keep='first')
+        self.advanced = self.advanced[self.advanced.Team.str.contains('TOT') == False]
+        self.dvanced = self.advanced[self.advanced.Team.str.contains('TM') == False]
+        self.advanced = self.advanced.drop(columns=["Pos", "WS", "Awards", "GS"])
+        self.processed = self.advanced[self.advanced.Player.str.contains('League') == False]
+        return self.processed
+    
+    def merge_stats_mvps(self) -> pd.DataFrame:
+        self.processed = pd.merge(self.processed, self.mvps, on=['Year', 'Player'], how="left")
+        self.processed.fillna(0, inplace=True)
+        return self.processed
+    
+    def remove_bad_players(self) -> pd.DataFrame:
+        self.processed = self.processed[self.processed["PER"] > 18]
+        self.processed = self.processed[self.processed["W/L%"] > 0.5]
+        self.processed = self.processed[self.processed["MP"] > 30]
+        return self.processed
+    
+    def scale(self) -> pd.DataFrame:
+        scaler = StandardScaler()
+        print(self.processed.head(5))
+        processed = self.processed.groupby('Year').apply(lambda x: pd.DataFrame(scaler.fit_transform(x.drop(columns=["Player", "Year", "Team"])), columns=x.columns.drop(["Player", "Year", "Team"])))
+        processed = processed.reset_index(drop=True)
+        processed[["Player", "Year", "Team"]] = self.processed[["Player", "Year", "Team"]].reset_index(drop=True)
+        self.processed = processed
+        return self.processed
+    
+    def process_with_mvps(self) -> pd.DataFrame:
+        self.remove_divisions()
+        self.clean_stats_standings()
+        self.update_team_names()
+        self.clean_mvps()
+        self.merge_stats()
+        self.merge_stats_mvps()
+        self.remove_bad_players()
+        self.scale()
+        return self.processed
+    
+    def process_without_mvps(self) -> pd.DataFrame:
+        self.remove_divisions()
+        self.clean_stats_standings()
+        self.update_team_names()
+        self.merge_stats()
+        self.remove_bad_players()
+        self.scale()
+        return self.processed
---
+++ src/scripts/Scrap.py
@@ -0,0 +1,110 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import time
+import pandas as pd
+
+class Scrap:
+    def __init__(self, first_year, last_year):
+        self.first_year = first_year
+        self.last_year = last_year
+    
+    def scrap_mvps(self) -> pd.DataFrame:
+        all_rows = []
+        for year in range(self.first_year, self.last_year + 1):
+            url = f"https://www.basketball-reference.com/awards/awards_{year}.html"
+            html = urlopen(url)
+            soup = BeautifulSoup(html, "lxml")
+            table = soup.find("table")
+            headers = [th.getText() for th in table.findAll("tr", limit=2)[1].findAll("th")]
+            headers[0] = "Year"
+            rows = table.findAll('tr')[2:]
+            rows_data = [[td.getText() for td in rows[i].findAll('td')]
+                            for i in range(len(rows))]
+            for i in range(len(rows_data)):
+                rows_data[i].insert(0, year)
+            all_rows.extend(rows_data)
+            print("MVPs for Season", year, "done")
+            time.sleep(5)
+        mvps = pd.DataFrame(all_rows, columns = headers)
+        mvps.to_csv(f"../data/raw/{self.first_year}_{self.last_year}_mvps.csv", index=False)
+        return mvps
+        
+    
+    def scrap_standings(self) -> pd.DataFrame:
+        all_rows = pd.DataFrame()
+        for year in range(self.first_year, self.last_year + 1):
+            url = f"https://www.basketball-reference.com/leagues/NBA_{year}_standings.html#expanded_standings"
+            html = urlopen(url)
+            soup = BeautifulSoup(html, "lxml")
+            tables = soup.findAll("table")
+            east_table = tables[0]
+            west_table = tables[1]
+            east_data, headers = self.scrap_conference(east_table, year)
+            west_data, _ = self.scrap_conference(west_table, year)
+            east = pd.DataFrame(east_data, columns = headers)
+            west = pd.DataFrame(west_data, columns = headers)
+            league = pd.concat([east, west])
+            league = league.sort_values(by=["W/L%"], ascending=False)
+            league["seed"] = range(1, len(league) + 1)
+            all_rows = pd.concat([all_rows, league])
+            print("Standing for season", year, "done")
+            time.sleep(5)
+        all_rows.to_csv(f"../data/raw/{self.first_year}_{self.last_year}_standings.csv", index=False)
+        return all_rows
+        
+    
+    def scrap_conference(self, table, year) -> pd.DataFrame:
+        headers = [th.getText() for th in table.findAll("tr", limit=2)[0].findAll("th")]
+        headers[0] = "Year"
+        headers.insert(1, "Team")
+        rows = table.findAll('tr')[1:]
+        rows_data = [[td.getText() for td in rows[i].findAll('td')]
+                    for i in range(len(rows))]
+        teams = [[td.getText() for td in rows[i].findAll('th')]
+                for i in range(len(rows))]
+        for i in range(len(rows_data)):
+                rows_data[i].insert(0, year)
+                rows_data[i].insert(1, teams[i][0].replace("*", ""))
+        return rows_data, headers
+    
+    def scrap_advanced(self) -> pd.DataFrame:
+        all_rows = []
+        for year in range(self.first_year, self.last_year + 1):
+            url = f"https://www.basketball-reference.com/leagues/NBA_{year}_advanced.html"
+            html = urlopen(url)
+            soup = BeautifulSoup(html, "lxml")
+            table = soup.find("table")
+            headers = [th.getText() for th in table.findAll("tr", limit=2)[0].findAll("th")]
+            headers[0] = "Year"
+            rows = table.findAll('tr')[1:]
+            rows_data = [[td.getText() for td in rows[i].findAll('td')]
+                            for i in range(len(rows))]
+            for i in range(len(rows_data)):
+                rows_data[i].insert(0, year)
+            all_rows.extend(rows_data)
+            print("Advanced for season", year, "done")
+            time.sleep(5)
+        advanced = pd.DataFrame(all_rows, columns = headers)
+        advanced.to_csv(f"../data/raw/{self.first_year}_{self.last_year}_advanced.csv", index=False)
+        return advanced
+        
+    def scrap_per_game(self) -> pd.DataFrame:
+        all_rows = []
+        for year in range(self.first_year, self.last_year + 1):
+            url = f"https://www.basketball-reference.com/leagues/NBA_{year}_per_game.html"
+            html = urlopen(url)
+            soup = BeautifulSoup(html, "lxml")
+            table = soup.find("table")
+            headers = [th.getText() for th in table.findAll("tr", limit=2)[0].findAll("th")]
+            headers[0] = "Year"
+            rows = table.findAll('tr')[1:]
+            rows_data = [[td.getText() for td in rows[i].findAll('td')]
+                            for i in range(len(rows))]
+            for i in range(len(rows_data)):
+                rows_data[i].insert(0, year)
+            all_rows.extend(rows_data)
+            print("Per game for season", year, "done")
+            time.sleep(5)
+        per_game = pd.DataFrame(all_rows, columns = headers)
+        per_game.to_csv(f"../data/raw/{self.first_year}_{self.last_year}_per_game.csv", index=False)
+        return per_game
