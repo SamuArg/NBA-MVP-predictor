be2353d42c9d131d2616910231aa97d930e3ff47
SamA1430 <samuel.arg14@gmail.com>
2025-03-27 19:12:57 -0400

    Fix wrong route to go to index

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -11,7 +11,8 @@ import { routes } from 'vue-router/auto-routes';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: setupLayouts(routes),
+  routes: setupLayouts([...routes,
+    { path: '/:pathMatch(.*)*', redirect: '/' }]),
 });
 
 // Workaround for https://github.com/vitejs/vite/issues/11804
